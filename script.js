b3d707c79708e4489657b754d67f08382d2b9ab3
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2022-10-15 16:57:52 -0400

    working through the arrays

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment code here
 
+const special = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -11,6 +15,9 @@ function writePassword() {
 
   passwordText.value = password;
 
+  if (x >= 8 && x <= 129) {
+
+  }
 }
 
 // Add event listener to generate button
