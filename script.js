4e2f571682e965292e6866fa932fd7e2044413dc
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2022-10-16 15:15:17 -0400

    Help from tutor; better understanding of JavaScript

--- script.js
+++ script.js
@@ -8,6 +8,48 @@ const uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  const length = window.prompt("What is the length of the password you want?");
+  console.log(length);
+
+  if (isNaN(length)) {
+    window.alert("Not a number!");
+    return
+  }
+
+  if (length < 8 || length > 128) {
+    window.alert("Wrong size!");
+    return
+  }
+
+  const possChar = [];
+    if (isUppercase) {
+      possChar.push(uppercase);
+    }
+    if (isLowercase) {
+      possChar.push(lowercase);
+    }
+    if (isSpecial) {
+      possChar.push(special);
+    }
+    if (isNumbers) {
+      possChar.push(numbers);
+    }
+
+
+  const isUppercase = window.confirm("Would you like to include uppercase?");
+  console.log(isUppercase);
+  const isLowercase = window.confirm("Would you like to include lowercase?");
+  console.log(isLowercase);
+  const isSpecial = window.confirm("Would you like to include special characters?");
+  console.log(isSpecial);
+  const isNumbers = window.confirm("Would you like to include numbers?");
+  console.log(isNumbers);
+  return
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,7 +57,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-  if (x >= 8 && x <= 129) {
+  if (x >= 8 && x <= 128) {
 
   }
 }
